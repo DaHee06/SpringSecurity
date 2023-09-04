37405e8e70e67e011fc1f6d655382fa7a5a06c25
Kimda <todayda1006@gmail.com>
2023-09-04 16:54:18 +0900

    Role 문제까지

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.springsecurity.config;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,6 +17,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    BoardUserDetailsService boardUserDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,10 +49,18 @@ public class SecurityConfig {
                         .loginPage("/login")    //로그인 페이지 설정
                         .loginProcessingUrl("/login")    //로그인 처리 URL 설정
                         .defaultSuccessUrl("/loginSuccess", true)  //로그인 성공 후 이동할 페이지
+//                        .successHandler(authSuccessHandler)
+//                        .failureHandler(authFailureHandler)
                         .permitAll()
                 )
+                .userDetailsService(boardUserDetailsService)
                 .logout(withDefaults());
 
         return http.build();
     }
+
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
--- src/main/java/com/example/springsecurity/config/SecurityUser.java
+++ src/main/java/com/example/springsecurity/config/SecurityUser.java
@@ -4,6 +4,7 @@ import com.example.springsecurity.domain.Member;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
+
 public class SecurityUser extends User {
 	private static final long serialVersionUID = 1L;
 
