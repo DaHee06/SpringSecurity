90a7265be6d04bc025d53a5635e04ce140fc1169
KimCome <todayda1006@gmail.com>
2023-09-20 22:55:40 +0900

    OAuth 기본 설정

--- src/main/java/com/example/springsecurity/domain/User.java
+++ src/main/java/com/example/springsecurity/domain/User.java
@@ -29,10 +29,28 @@ public class User implements UserDetails {
     @Column(name = "password", nullable = false)
     private String password;
 
+    //사용자 이름
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+//    @Builder
+//    public User(String email, String password, String auth) {
+//        this.email = email;
+//        this.password = password;
+//    }
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
+    }
+
+    //사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 
 
